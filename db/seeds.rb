3566d2d5fc2c951b62f5924e5e500cb3c4a26a41
Matthew Miner <misternuks@gmail.com>
2024-02-07 21:55:31 +0900

    seed the database; add routes and beers controller with index and show

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Inserting the breakfast pastry..."
+Beer.destroy_all if Rails.env.development?
+Category.destroy_all if Rails.env.development?
+
+puts "Contact Dr. Jemima..."
+ipa = Category.create!(name: 'IPA')
+stout = Category.create!(name: "stout")
+sour = Category.create!(name: "sour")
+
+puts "Start warming the syrup..."
+Beer.create!(sku: "ipa-sto-enj-001", name: "Stone Enjoy By 04.20.23", category: ipa, photo_url: ActionController::Base.helpers.image_path("stone_enjoy_by.jpg"))
+
+Beer.create!(sku: "stout-fre-bbo-001", name: "Fremont B-Bomb", category: stout, photo_url: ActionController::Base.helpers.image_path("fremont_b_bomb.jpeg"))
+
+Beer.create!(sku: "sour-gre-dou-001", name: "Great Notion Double Blueberry Shake", category: sour, photo_url: ActionController::Base.helpers.image_path("great_notion_double.jpeg"))
+
+puts "Oh my God, my waffle!"
